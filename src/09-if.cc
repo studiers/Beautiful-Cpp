597496b53787f61449c325cdf327428151a28589
moreal <dev.moreal@gmail.com>
2018-07-04 13:01:47 +0900

    ADD: If

---
+++ src/09-if.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+auto main() -> int {
+
+    if (1 == 1) {
+        // CODE
+    } else if (2 == 2) {
+        // CODE
+    } else {
+        // CODE
+    }
+
+    return 0;
+}
